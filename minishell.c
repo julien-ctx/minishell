4a0636dcf9927d21175303443a02a47458be01d8
Julien CAUCHETEUX <jcmcshopy@gmail.com>
2022-06-12 17:05:44 +0200

    Fixed prompt leak

--- minishell.c
+++ minishell.c
@@ -3,16 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcauchet <jcauchet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: juliencaucheteux <juliencaucheteux@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 18:39:13 by jcauchet          #+#    #+#             */
-/*   Updated: 2022/06/11 20:33:20 by jcauchet         ###   ########.fr       */
+/*   Updated: 2022/06/12 17:05:27 by juliencauch      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
 int	g_global = 0;
+
+int	return_and_free(char *prompt)
+{
+	free(prompt);
+	return (STOP);
+}
+
 /***************************************************************************
 Uses global variable to checkk if SIGINT was used. Checks if CTRL-D has
 been typed by checking args value (NULL when you type CTRL-D)
@@ -40,7 +47,7 @@ int stop_check(char *args, char *prompt)
 Main loop to retrieve arguments and then use the parser or display an error
 message on the terminal and terminate the session
 ***************************************************************************/
-void	req_args(void)
+int	req_args(void)
 {
 	char	*args;
 	char	*prompt;
@@ -52,7 +59,7 @@ void	req_args(void)
 		if (ft_strlen(args))
 			add_history(args);
 		if (stop_check(args, prompt) == STOP)
-			return ;
+			return(return_and_free(prompt));
 		else
 			continue;
 		free(prompt);
